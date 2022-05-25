e2669080b8017bec29fe1b3169ef1c9fe84f9a53
Trevor <trevorward001@gmail.com>
2022-05-24 18:19:58 -0600

    Two Sum completed

---
+++ solutions/twoSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
