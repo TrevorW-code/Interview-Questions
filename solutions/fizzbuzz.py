dd2932a472e079bac6eeeb43ec6f5a0dd104a2a8
Trevor <trevorward001@gmail.com>
2023-01-24 15:57:33 -0700

    added fizzbuzz, little memory option

---
+++ solutions/fizzbuzz.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+
+        answer = []
+
+        for i in range(1,n+1):
+            if (i%15==0):
+                answer.append("FizzBuzz")
+            elif i%3==0:
+                answer.append("Fizz")
+            elif i%5==0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+
+        return answer
